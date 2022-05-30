e55c08a25d888df73a11fdeaf0ffd7c457442707
Eli Gundry <eligundry@gmail.com>
2022-05-30 04:49:29 -0400

    Add read-more breakpoint remark plugin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const visit = require('unist-util-visit');
 const isComment = new RegExp('<!--(.*?)-->');
 const getComment = new RegExp('<!--([\\s\\S]*?)-->');
 
-const plugin = (options = {}) => {
+const excerpt = (options = {}) => {
     const transformer = (tree) => {
         const excerpts =
             options.identifier && options.identifier.length
@@ -34,4 +34,40 @@ const plugin = (options = {}) => {
     return transformer;
 };
 
-module.exports = plugin;
+/**
+ * @type {import('unified').Plugin}
+ */
+const excerptBreakpoint = (options = {}) => {
+    return (tree) => {
+        const excerpts =
+            options.identifier && options.identifier.length
+                ? [options.identifier]
+                : ['excerpt', 'more', 'preview', 'teaser'];
+
+        let excerptIndex = -1;
+
+        visit(tree, 'comment', (node, idx) => {
+            if (node.commentValue && excerpts.some((e) => node.commentValue.trim() === e)) {
+                excerptIndex = idx;
+            }
+        });
+
+        if (excerptIndex > -1) {
+            tree.children[excerptIndex] = {
+                type: 'mdxJsxTextElement',
+                name: 'span',
+                attributes: [
+                    {
+                        type: 'mdxJsxAttribute',
+                        name: 'id',
+                        value: 'read-more'
+                    }
+                ],
+                children: [],
+                data: {_mdxExplicitJsx: true}
+            };
+        }
+    };
+};
+
+module.exports = {excerpt, excerptBreakpoint};
